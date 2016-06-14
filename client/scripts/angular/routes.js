a69b99d802b8dd80f15fdd64e737dfe67fb6a152
Jed <brun7695@stthomas.edu>
2016-06-14 16:47:24 -0500

    modal and slider working and configured. path change on close

--- client/scripts/angular/routes.js
+++ client/scripts/angular/routes.js
@@ -61,19 +61,67 @@ portfolio.directive('gallery', function ($timeout) {
 portfolio.controller('knabbaSliderController', function ($scope) {
     $scope.images = [{
         src: '../images/projects/knabba/KnabbaMainScreen.PNG',
-        title: 'First Image'
+        title: 'Knabba Main Screen'
     }, {
-        src: 'apple_2-wallpaper-1920x1080.jpg',
-        title: 'Image Dos'
+        src: '../images/projects/knabba/KnabbaMainScreen2PNG.PNG',
+        title: 'Knabba Main Screen 2'
     }, {
-        src: 'apple_desktop-wallpaper-1920x1080.jpg',
-        title: 'Tre'
+        src: '../images/projects/knabba/KnabbaMainScreen3.PNG',
+        title: 'Knabba Main Screen 3'
     }, {
-        src: 'apple_ios_snow_mountains-wallpaper-1920x1080.jpg',
-        title: 'Numero 4'
+        src: '../images/projects/knabba/KnabbaGetStarted.PNG',
+        title: 'Login/Sign up'
+    }, {
+        src: '../images/projects/knabba/KnabbaCreateAccount.PNG',
+        title: 'Create Account'
+    }, {
+        src: '../images/projects/knabba/KnabbaRenter1.PNG',
+        title: 'Define Account'
+    }, {
+        src: '../images/projects/knabba/KnabbaRenter2.PNG',
+        title: 'Define Account 2'
+    }, {
+        src: '../images/projects/knabba/KnabbaRenter3PNG.PNG',
+        title: 'Define Account 3'
+    }, {
+        src: '../images/projects/knabba/KnabbaRenter4.PNG',
+        title: 'Define Account 4'
+    }, {
+        src: '../images/projects/knabba/KnabbaProcessedScript.PNG',
+        title: 'Initial Processing Script'
+    }, {
+        src: '../images/projects/knabba/KnabbaUploadScript.PNG',
+        title: 'File Upload Script'
+    }, {
+        src: '../images/projects/knabba/KnabbaGalleryScript.PNG',
+        title: 'View Photo Gallery Script'
+    }, {
+        src: '../images/projects/knabba/KnabbaOffersScript.PNG',
+        title: 'Offers Script'
     }];
 });
 
+portfolio.controller('carischSliderController', function ($scope) {
+    $scope.images = [{
+        src: '../images/projects/carischApp/AppIndex.PNG',
+        title: 'Main Page'
+    }, {
+        src: '../images/projects/carischApp/AppContext.PNG',
+        title: 'Context Code'
+    }, {
+        src: '../images/projects/carischApp/AppModel.PNG',
+        title: 'Model Code'
+    }, {
+        src: '../images/projects/carischApp/AppRoutes.PNG',
+        title: 'Routes'
+    }, {
+        src: '../images/projects/carischApp/AppController1.PNG',
+        title: 'Controller'
+    }, {
+        src: '../images/projects/carischApp/AppController2.PNG',
+        title: 'Controller 2'
+    }];
+});
 
 
 portfolio.controller('knabbaModalController', ['$uibModal', function ($uibModal) {
@@ -87,10 +135,39 @@ portfolio.controller('knabbaModalController', ['$uibModal', function ($uibModal)
         };
 }]);
 
-portfolio.controller('closeController', ['$uibModalInstance', function ($uibModalInstance) {
+portfolio.controller('carischModalController', ['$uibModal', function ($uibModal) {
+    var vm = this;
+    vm.open = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: '../views/carischModal.html',
+            controller: 'closeController as vm',
+            windowClass: 'app-modal-window'
+        });
+    };
+}]);
+
+portfolio.controller('closeController', ['$uibModalInstance', '$location', function ($uibModalInstance, $location) {
+
        var vm = this;
 
         vm.close = function () {
             $uibModalInstance.close();
+            $location.path('/projects');
         };
+
 }]);
+
+
+portfolio.run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
+    var original = $location.path;
+    $location.path = function (path, reload) {
+        if (reload === false) {
+            var lastRoute = $route.current;
+            var un = $rootScope.$on('$locationChangeSuccess', function () {
+                $route.current = lastRoute;
+                un();
+            });
+        }
+        return original.apply($location, [path]);
+    };
+}])

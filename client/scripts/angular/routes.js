2ee784cf6c619ccd50f1dbf06b90b6e9317c4839
Jed <brun7695@stthomas.edu>
2016-06-14 16:52:01 -0500

    modal and slider working and configured. path change on close

--- client/scripts/angular/routes.js
+++ client/scripts/angular/routes.js
@@ -157,17 +157,3 @@ portfolio.controller('closeController', ['$uibModalInstance', '$location', funct
 
 }]);
 
-
-portfolio.run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
-    var original = $location.path;
-    $location.path = function (path, reload) {
-        if (reload === false) {
-            var lastRoute = $route.current;
-            var un = $rootScope.$on('$locationChangeSuccess', function () {
-                $route.current = lastRoute;
-                un();
-            });
-        }
-        return original.apply($location, [path]);
-    };
-}])
